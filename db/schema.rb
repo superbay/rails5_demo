720e08973e4736b1ae3e9c06b35713ac0ed54ad0
xiongbo <xiongbo@gmail.com>
2016-05-15 00:17:03 +0000

    added relation for post

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514233429) do
+ActiveRecord::Schema.define(version: 20160515001625) do
 
-  create_table "profiles", force: true do |t|
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
-
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
     t.datetime "created_at"
